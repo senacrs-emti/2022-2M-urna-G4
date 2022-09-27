f360cb14228693ce57b75aa4a50d309b76d2371b
Lucas <estudantesenac@senacrs.edu.br>
2022-09-27 11:28:29 -0300

    tweak: Adicionado estrutura inicial do projeto.

---
+++ admin/includes/controllers/Database.php
@@ -0,0 +1,29 @@
+<?php
+
+class Database {
+    private $DB_USER = "root";
+    private $DB_NAME = "urna";
+    private $DB_HOST = "localhost";
+    private $DB_PASS = "";
+
+    public $pdo;
+
+    public function __construct() {
+        try {
+            $this->pdo = new PDO("mysql:dbname=$this->DB_NAME;host=$this->DB_HOST", $this->DB_USER, $this->DB_PASS);
+        } catch (PDOException $e) {
+            print("Unable to establish database connection ".$e->getMessage());
+        } catch (Exception $e) {
+            print("Error ".$e->getMessage());
+        }
+    }
+
+    public function Query($sql, $params = []) {
+        $query = $this->pdo->prepare($sql);
+
+        $query->execute($params);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+}
