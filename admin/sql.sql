2dbda4a43f3f3c6e794b316aa2fb49fa070168ba
Lucas <neexdeveloper@gmail.com>
2022-10-11 10:55:26 -0300

    chore(sql): Add new sql schema

--- admin/sql.sql
+++ admin/sql.sql
@@ -11,10 +11,17 @@ CREATE TABLE parties (
   `website_url` VARCHAR(255)
 );
 
+CREATE TABLE roles (
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT, 
+  `name` VARCHAR(255)
+);
+
 CREATE TABLE candidates (
   `id` INTEGER PRIMARY KEY AUTO_INCREMENT, 
   `name` VARCHAR(255), 
   `number` INTEGER,
   `party_id` INTEGER, 
-  foreign key (party_id) references parties (id)
+  `role_id` INTEGER,
+  foreign key (party_id) references parties (id),
+  foreign key (role_id) references roles (id)
 );
